ba4749c703e0d247847d3449c1e5b3f4008974ce
JiyeonChoi97 <57701500+JiyeonChoi97@users.noreply.github.com>
2020-04-09 14:24:10 +0900

    07-casting

---
+++ 07-casting/exam1.py
@@ -0,0 +1,19 @@
+'''
+bool(데이터) : 데이터가 있는지 없는지를 검사
+    --> True : 데이터가 있음, False : 데이터가 없음
+'''
+# 숫자
+print(bool(100))
+print(bool(0))
+print('-'*40)
+
+# 문자열
+print(bool('Hello'))
+print(bool(''))
+print('-'*40)
+
+# None
+print(bool(None))
+print('-'*40)
+
+
