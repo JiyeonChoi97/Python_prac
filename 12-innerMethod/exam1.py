aeb4d3ec6ffd59a6a63d23f0991fec9a64299629
JiyeonChoi97 <57701500+JiyeonChoi97@users.noreply.github.com>
2020-04-10 14:36:14 +0900

    12-innerMethod

---
+++ 12-innerMethod/exam1.py
@@ -0,0 +1,10 @@
+score = [70, 80, 90, 87]
+print('score : ', score)
+print('총점 : ', sum(score), ', 평균 : ', sum(score)/len(score))
+print('최대값 : ', max(score), ', 최소값 : ', min(score))
+
+'''
+score :  [70, 80, 90, 87]
+총점 :  327 , 평균 :  81.75
+최대값 :  90 , 최소값 :  70
+'''
